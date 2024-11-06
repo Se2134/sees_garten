7dad7cf1ae9b5dbf7f50c389a550e49f61b3e7e1
se2134 <Nack.seb@gmail.com>
2024-11-06 21:33:43 +0100

    DB anfang

---
+++ db/config.ts
@@ -0,0 +1,57 @@
+import { column, defineDb, defineTable } from 'astro:db';
+
+
+const Plants = defineTable({
+  columns: {
+    idplants: column.number({primaryKey: true}),
+    name: column.text({default: "/"}),
+    lat_name: column.text({default: "/"}),
+    type: column.text({default: "/"}),
+    size: column.text({default: "/"}),
+    propergation: column.text({default: "/"}),
+    light: column.text({default: "/"}),
+    water: column.text({default: "/"}),
+    notes: column.text({default: "/"})
+  }
+})
+
+const Projects = defineTable({
+  columns: {
+    idprojects: column.number({primaryKey: true}),
+    name: column.text({default: "/"}),
+    container: column.number({references: () => Equipment.columns.idequipment, default: 0}),
+    plants: column.number({references: () => Plants.columns.idplants, default: 0}),
+    animals: column.number({references: () => Animals.columns.idanimals, default: 0}),
+    notes: column.text({default: "/"})
+  }
+})
+
+const Animals = defineTable({
+  columns: {
+    idanimals: column.number({primaryKey: true}),
+    name: column.text({default: "/"}),
+    lat_name: column.text({default: "/"}),
+    size: column.text({default: "/"}),
+    food: column.text({default: "/"}),
+    reproduction: column.text({default: "/"}),
+    type: column.text({default: "/"}),
+    notes: column.text({default: "/"})
+  }
+})
+
+const Equipment = defineTable({
+  columns: {
+    idequipment: column.number({primaryKey: true}),
+    name: column.text({default: "/"}),
+    price: column.text({default: "/"}),
+    link: column.text({default: "/"}),
+    type: column.text({default: "/"}),
+    notes: column.text({default: "/"})
+  }
+})
+
+
+// https://astro.build/db/config
+export default defineDb({
+  tables: { Plants, Animals, Equipment, Projects },
+});
---
+++ db/seed.ts
@@ -0,0 +1,25 @@
+import { db, Plants, Animals, Equipment, Projects } from 'astro:db';
+
+// https://astro.build/db/seed
+export default async function seed() {
+
+	await db.insert(Plants).values([
+		{name: "Monstera Deliciosa db", type: "aeroid",},
+		{name: "Monsti", type: "aeroid",}
+	])
+
+	await db.insert(Animals).values([
+		{name: "Monstera Deliciosa db", type: "aeroid",},
+		{name: "Monsti", type: "aeroid",}
+	])
+
+	await db.insert(Equipment).values([
+		{name: "Monstera Deliciosa db", type: "aeroid",},
+		{name: "Monsti", type: "aeroid",}
+	])
+
+	await db.insert(Projects).values([
+		{name: "Monstera Deliciosa db"},
+		{name: "Monsti"}
+	])
+}
