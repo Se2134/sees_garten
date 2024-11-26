cd9c538c8a806e91fab78aa58a5827996a5ba244
se2134 <Nack.seb@gmail.com>
2024-11-26 10:54:45 +0100

    [slug].astro site added

--- db/seed.ts
+++ db/seed.ts
@@ -4,22 +4,20 @@ import { db, Plants, Animals, Equipment, MyProjects } from 'astro:db';
 export default async function seed() {
 
 	await db.insert(Plants).values([
-		{name: "Monstera Deliciosa db", type: "aeroid",},
-		{name: "Monsti", type: "aeroid",}
+		{name: "Monstera Deliciosa", type: "aeroid",},
+		{name: "Philodendron Verrucosum", type: "aeroid",}
 	])
 
 	await db.insert(Animals).values([
-		{name: "Monstera Deliciosa db", type: "aeroid",},
-		{name: "Monsti", type: "aeroid",}
+		{name: "Honiggurami", type: "labyrinthfish",},
+		{name: "Perlhuhnbärbling", type: "rasbora",}
 	])
 
 	await db.insert(Equipment).values([
-		{name: "Monstera Deliciosa db", type: "aeroid",},
-		{name: "Monsti", type: "aeroid",}
+		{name: "Aquarium 120L", type: "container",},
 	])
 
 	await db.insert(MyProjects).values([
-		{name: "Monstera Deliciosa db", type: "aeroid"},
-		{name: "Monsti", type: "aeroid"}
+		{name: "Altes AQ 2.0", type: "aquarium"},
 	])
 }
